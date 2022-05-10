dcfd937c7fd4e1c0297b60f5a5154e39eabb5a3b
Zitan Salehin <zitan-74@dipti.com.bd>
2022-05-10 17:02:52 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-app';
+
+  countNumber=0;
+
+  resetBtn(){
+    this.countNumber=0;
+    
+  }
+  increaseBtn(){
+    this.countNumber=this.countNumber+1;
+
+  }
+  decreaseBtn(){
+    this.countNumber=this.countNumber-1;
+
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
